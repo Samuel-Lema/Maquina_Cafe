5f2bc44ed0325f51804caf9e7f9cd95a710889a5
slemagonzalez <slemagonzalez@victoria19.danielcastelao.org>
2018-02-16 09:13:17 +0100

    Creada Clase Display (Gestiona la interancion de mensajes hacia el Usuario)

---
+++ src/maquina_cafe/Display.java
@@ -0,0 +1,15 @@
+package maquina_cafe;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author slemagonzalez
+ */
+public class Display {
+    
+    public static void window(String mensaje){
+        
+        JOptionPane.showMessageDialog(null, mensaje);
+    }
+}
